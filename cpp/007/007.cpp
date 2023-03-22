070239d7f133b2b9893e4425d22c5c0c5a145207
czlabinger <czlabinger@student.tgm.ac.at>
2023-03-22 15:37:05 +0100

    C++ null pointer

---
+++ cpp/007/007.cpp
@@ -0,0 +1,29 @@
+// nullptr
+#include <iostream>
+#include <new>
+using namespace std;
+int main ()
+{
+  int i = 0;
+  int n = 0;
+  int * p;
+  cout << "How many numbers would you like to type? ";
+  cin >> i;
+
+  p = new (nothrow) int[i];
+  if (p == nullptr)
+    cout << "Error: memory could not be allocated";
+  else
+  {
+    for (n=0; n<i; n++)
+    {
+      cout << "Enter number: ";
+      cin >> p[n];
+    }
+    cout << "You have entered: ";
+    for (n=0; n<i; n++)
+      cout << p[n] << ", ";
+    delete[] p;
+  }
+  return 0;
+}
